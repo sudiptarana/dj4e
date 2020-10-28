5d741602714825aaba2780354dab3829149a67e7
sudiptarana <sudiptarana00@gmail.com>
2020-10-28 10:20:16 +0530

    up

--- unesco/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Category, States, Region, Iso, Site
-# Register your models here.
-admin.site.register(Category)
-admin.site.register(States)
-admin.site.register(Region)
-admin.site.register(Iso)
-admin.site.register(Site)
